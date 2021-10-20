a9e5942edd9212288189eeaa01ff2abee0a0f545
Viacheslav Panasiuk <laburunt@gmail.com>
2021-10-20 22:34:53 +0300

    final

--- src/app.js
+++ src/app.js
@@ -1,5 +1,54 @@
-new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2019'),
+const dayTimer = document.querySelector('span[data-value="days"]');
+const hourTimer = document.querySelector('span[data-value="hours"]');
+const minTimer = document.querySelector('span[data-value="mins"]');
+const secTimer = document.querySelector('span[data-value="secs"]');
+
+class Timer {
+  constructor(finishDate, markup) {
+    this.markup = markup;
+    this.finishDate = finishDate;
+    this.interval = null;
+    this.deltaTime = null;
+  }
+
+  start() {
+    this.interval = setInterval(() => {
+      let currentTime = Date.now();
+      this.deltaTime = this.finishDate - currentTime;
+
+      const days = this.pad(Math.floor(this.deltaTime / (1000 * 60 * 60 * 24)));
+      
+      const hours = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      );
+
+      const mins = this.pad(
+        Math.floor((this.deltaTime % (1000 * 60 * 60)) / (1000 * 60)),
+      );
+
+      const sec = this.pad(Math.floor((this.deltaTime % (1000 * 60)) / 1000));
+      this.insertValues(days, hours, mins, sec);
+    }, 1000);
+  }
+
+  pad(value) {
+    return String(value).padStart(2, '0');
+  }
+
+  insertValues(d, h, m, s) {
+    const { dayTimer, hourTimer, minTimer, secTimer } = this.markup;
+    dayTimer.textContent = d;
+    hourTimer.textContent = h;
+    minTimer.textContent = m;
+    secTimer.textContent = s;
+  }
+}
+
+const myTimer = new Timer(new Date('march 27, 2022'), {
+  dayTimer,
+  hourTimer,
+  minTimer,
+  secTimer,
 });
 
+myTimer.start();
